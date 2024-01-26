143b59454c81557a8fb6ca0818a5021fcf5949b3
GUEST-1001 <87338258+GUEST-1001@users.noreply.github.com>
2024-01-26 22:49:10 +0700

    Add SimpleSceneChange To Player Prefab

---
+++ Assets/Script/SimpleSceneChange.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimpleSceneChange : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isLocalPlayer)
+        {
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                CallLaodSceneCMD();
+            }
+        }
+    }
+
+    [Command]
+    void CallLaodSceneCMD()
+    {
+        CallLaodScene();
+    }
+
+    [ClientRpc]
+    void CallLaodScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
