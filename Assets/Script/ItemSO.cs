67c00693c7dfa642ab7f8b3e376ffb0ee17324df
topr.nontawat <topr.nontawat@gmail.com>
2024-01-26 13:45:52 +0700

    Project Resetting

---
+++ Assets/Script/ItemSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemSO", menuName = "Items/ItemSO", order = 0)]
+public class ItemSO : ScriptableObject {
+    public byte itemID;
+    public string itemName;
+}
