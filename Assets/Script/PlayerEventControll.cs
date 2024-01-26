4b0ac99221e19f49b8e4b80c9f9cf29d7f3c8436
GUEST-1001 <87338258+GUEST-1001@users.noreply.github.com>
2024-01-26 23:57:10 +0700

    Test PlayerContoll system

--- Assets/Script/PlayerEventControll.cs
+++ Assets/Script/PlayerEventControll.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 
-public class PlayerEventControll : MonoBehaviour
+public class PlayerEventControll : NetworkBehaviour
 {
+    PlayerStatus playerStatus;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (this.isLocalPlayer)
+        {
+            if (playerStatus.sunflower == 10)
+            {
+                Debug.Log("Now My Sunflower is 10");
+            }
+        }
     }
 }
