8275068c04796a7d2d703af0dfd1629edd79e679
GUEST-1001 <87338258+GUEST-1001@users.noreply.github.com>
2024-01-27 23:19:51 +0700

    Basic interact Script

--- Assets/Script/InteractContoller.cs
+++ Assets/Script/InteractContoller.cs
@@ -5,16 +5,51 @@ using UnityEngine;
 
 public class InteractContoller : NetworkBehaviour
 {
-    [SerializeField] [SyncVar] bool isOnUse;
+    [SerializeField][SyncVar] public bool isOnUse;
+    [SerializeField] GameObject fButton;
+    [SerializeField] GameObject isUseButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (isOnUse)
+                other.GetComponent<PlayerInteractContoller>().SetActiveInteract(isUseButton, true);
+            else
+                other.GetComponent<PlayerInteractContoller>().SetActiveInteract(fButton, true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerInteractContoller>().SetActiveInteract(isUseButton, false);
+            other.GetComponent<PlayerInteractContoller>().SetActiveInteract(fButton, false);
+        }
+    }
+
+    [Command(requiresAuthority = false)]
+    public void IsOnUseFilp()
+    {
+        isOnUse = !isOnUse;
+    }
+    [Command(requiresAuthority = false)]
+    public void IsOnUseFilp(bool value)
+    {
+        isOnUse = value;
+    }
+
 }
---
+++ Assets/Script/PlayerInteractContoller.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class PlayerInteractContoller : NetworkBehaviour
+{
+    [SerializeField] GameObject tempInteractOBJ;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            if (tempInteractOBJ != null)
+            {
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    tempInteractOBJ.GetComponent<InteractContoller>().IsOnUseFilp();
+                }
+            }
+        }
+    }
+
+    public void SetActiveInteract(GameObject obj, bool value)
+    {
+        if (isLocalPlayer)
+            obj.SetActive(value);
+    }
+
+    // private void OnTriggerStay2D(Collider2D other)
+    // {
+    //     if (isLocalPlayer)
+    //         if (other.CompareTag("InteractOBJ"))
+    //         {
+    //             if (Input.GetKeyDown(KeyCode.F))
+    //                 Debug.Log("Is on strry");
+    //         }
+    // }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isLocalPlayer)
+            if (other.CompareTag("InteractOBJ"))
+            {
+                if (!other.GetComponent<InteractContoller>().isOnUse)
+                    tempInteractOBJ = other.gameObject;
+            }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (isLocalPlayer)
+            if (other.CompareTag("InteractOBJ"))
+            {
+                if (tempInteractOBJ != null)
+                    other.GetComponent<InteractContoller>().IsOnUseFilp(false);
+                tempInteractOBJ = null;
+            }
+    }
+}
