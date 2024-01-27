6c053783b6d1276e6d8b543d3468057add0e10fc
topr.nontawat <topr.nontawat@gmail.com>
2024-01-28 02:02:05 +0700

    Update GUI

--- Assets/Script/PlayerUI.cs
+++ Assets/Script/PlayerUI.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using Mirror;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerUI : NetworkBehaviour
 {
+    [Header("UI")]
     [SerializeField] private PlayerStatus playerStatus;
-    [SerializeField] private TextMeshProUGUI pulseTxt;
-    [SerializeField] private TextMeshProUGUI urgeTxt;
+    [SerializeField] private Image pulseImg;
+    [SerializeField] private Image urgeBar;
     [SerializeField] private TextMeshProUGUI sunflowerText;
+    [SerializeField] private Image timerArm;
     [SerializeField] private TextMeshProUGUI timeText;
     // Start is called before the first frame update
     void Start()
@@ -20,17 +23,24 @@ public class PlayerUI : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        pulseTxt.text = "Pulse : " + playerStatus.pulse.ToString();
-        urgeTxt.text = "Urge : " + playerStatus.urge.ToString();
-        sunflowerText.text = "Sunflower Seed : " + playerStatus.sunflower.ToString();
-        timeText.text = "Time : " + GameManager.instance.timeCount.ToString("0");
-        // GetNowTimeCMD();
+        sunflowerText.text = "x" + playerStatus.sunflower.ToString();
+        timeText.text = GameManager.instance.timeCount.ToString("0");
+        UpdateTime();
+        UpdateUrge();
+        UpdatePulse();
     }
 
-    [Command(requiresAuthority = false)]
-    void GetNowTimeCMD()
-    {
-        GetNowTime();
+    void UpdateTime(){
+        timerArm.transform.localRotation =  Quaternion.Euler(0,0,90f - GameManager.instance.timeCount);
+    }
+
+    void UpdateUrge(){
+        urgeBar.fillAmount = (float)playerStatus.urge / 100f;
+    }
+
+    void UpdatePulse(){
+        Animator animator = pulseImg.GetComponent<Animator>();
+        animator.speed = 1 + ((float)playerStatus.pulse - 300f) / 75f;
     }
 
     [ClientRpc]
